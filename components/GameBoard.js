f03fec1774376de2d7fdd521fb8bf1e8454afbc3
Fiona Wilson <fionaw237@gmail.com>
2020-09-25 17:50:02 +0100

    Initial work on adding state for grid cells

--- components/GameBoard.js
+++ components/GameBoard.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { View, StyleSheet } from 'react-native'
 
 import GridCell from './GridCell'
@@ -8,7 +8,29 @@ const GameBoard = props => {
     const numberOfRows = 9
     const numberOfColumns = 9
 
-    const createGameBoard = () => {
+    const initialiseGridCells = () => {
+        const result = []
+        for (let rowNumber = 0; rowNumber < numberOfRows; rowNumber++) {
+            for (let columnNumber = 0; columnNumber < numberOfColumns; columnNumber++)
+            result.push(
+                {
+                    coordinate: rowNumber.toString() + columnNumber.toString(),
+                    hasMine: false,
+                    hasFlag: false,
+                    uncovered: false
+                }
+            )
+        }
+        return result
+    }
+
+    const arrayIndexFromCoordinate = (coordinate) => {
+        return (parseInt(coordinate[0]) * numberOfColumns) + parseInt(coordinate[1])
+    }
+
+    const [gridCells, setGridCells] = useState(initialiseGridCells())
+
+    const renderGameBoard = () => {
         const rowsArray = []
         for (let rowNumber = 0; rowNumber < numberOfRows; rowNumber++) {
             rowsArray.push(
@@ -23,20 +45,25 @@ const GameBoard = props => {
     const createRow = (rowNumber) => {
         const rowArray = []
         for (let columnNumber = 0; columnNumber < numberOfColumns; columnNumber++) {
+            console.log("Coordinate: ", columnNumber.toString() + rowNumber.toString())
+
             rowArray.push(
-                <GridCell key={columnNumber.toString()} coordinate={columnNumber.toString() + rowNumber.toString()} onPress={handleCellPress}/>
+                <GridCell key={columnNumber.toString()} cell={gridCells[arrayIndexFromCoordinate(columnNumber.toString() + rowNumber.toString())]} onPress={handleCellPress}/>
             )
         }
         return rowArray
     }
 
     const handleCellPress = (coordinate) => {
-        console.log(coordinate)
+        gridCells[arrayIndexFromCoordinate(coordinate)].uncovered = true
+        // console.log(arrayIndexFromCoordinate(coordinate))
+        // console.log(gridCells[arrayIndexFromCoordinate(coordinate)])
+        setGridCells(current => [...current])
     }
 
     return (
         <View>
-            {createGameBoard()}
+            {renderGameBoard()}
         </View>
     )
 }
--- components/GridCell.js
+++ components/GridCell.js
@@ -1,20 +1,35 @@
-import React from 'react'
-import {TouchableOpacity, Image, StyleSheet} from 'react-native'
+import React from "react"
+import { TouchableOpacity, Image, StyleSheet, View } from "react-native"
+
+const configureCellDisplay = (cell) => {
+  if (!cell.uncovered) {
+    return <Image style={styles.cell} source={require("../assets/grid-cell-button.png")} />
+  }
+  return <View style={styles.uncoveredCell}></View>
+}
 
 const GridCell = (props) => {
+
+  console.log(props)
+
   return (
     <TouchableOpacity
-      onPress={() => props.onPress(props.coordinate)}>
-      <Image style = {styles.cell} source={require('../assets/grid-cell-button.png')} />
+      onPress={() => props.onPress(props.cell.coordinate)}>
+      {configureCellDisplay(props.cell)}
     </TouchableOpacity>
   )
 }
 
 const styles = StyleSheet.create({
-    cell: {
-        width: 40,
-        height: 40
-    }
+  cell: {
+    width: 40,
+    height: 40
+  },
+  uncoveredCell: {
+    width: 40,
+    height: 40,
+    backgroundColor: "black"
+  }
 })
 
 export default GridCell
