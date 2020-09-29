e32c3edbbf4714d1f4c6ee9869e7086ad97d2108
Fiona Wilson <fionaw237@gmail.com>
2020-09-29 20:28:16 +0100

    Added styling to nav bar

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -3,12 +3,38 @@ import { createStackNavigator } from 'react-navigation-stack'
 
 import WelcomeScreen from "../screens/WelcomeScreen"
 import GameScreen from "../screens/GameScreen"
-import BestTimesScreen from "../screens/BestTimesScreen" 
+import BestTimesScreen from "../screens/BestTimesScreen"
+
+import Colours from "../constants/colours"
 
 const AppNavigator = createStackNavigator({
-    Welcome: WelcomeScreen,
-    Game: GameScreen,
-    BestTimes: BestTimesScreen
-})
+    Welcome: {
+        screen: WelcomeScreen,
+        navigationOptions: {
+            headerTitle: ""
+        }
+    },
+    Game: {
+        screen: GameScreen,
+        navigationOptions: {
+            headerTitle: "Minesweeper"
+        }
+    },
+    BestTimes: {
+        screen: BestTimesScreen,
+        navigationOptions: {
+            headerTitle: "Best Times"
+        }
+    }
+},
+    {
+        mode: "modal",
+        defaultNavigationOptions: {
+            headerStyle: {
+                backgroundColor: Platform.OS === "android" ? Colours.primary : Colours.mainBackground
+            },
+            headerTintColor: Platform.OS === "android" ? "white" : Colours.primary
+        }
+    })
 
 export default createAppContainer(AppNavigator)
